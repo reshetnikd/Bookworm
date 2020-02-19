bb6a96f9d1894ad4c7c9367b378a66c52b15f588
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-19 21:22:56 +0200

    Try out some code

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,9 +8,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
     var body: some View {
-        Text("Hello, World!")
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var rememberMe = false
+//    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.horizontalSizeClass) var sizeClass
+    
+    var body: some View {
+//        VStack {
+//            PushButton(title: "Remember Me", isOn: $rememberMe)
+//            Text(rememberMe ? "On" : "Off")
+////            Text("Hello World")
+////                .onTapGesture {
+////                    self.presentationMode.wrappedValue.dismiss()
+////            }
+//            Spacer()
+            
+            if sizeClass == .compact {
+                return AnyView(VStack {
+                    Text("Active size class:")
+                    Text("COMPACT")
+                }
+                .font(.largeTitle))
+            } else {
+                return AnyView(HStack {
+                    Text("Active size class:")
+                    Text("REGULAR")
+                }
+                .font(.largeTitle))
+            }
+//        }
     }
 }
 
